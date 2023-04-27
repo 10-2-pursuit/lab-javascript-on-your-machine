9d26ed06b483856901675b5d0258ca0e002c6596
Michael Caldwell <michaelcaldwell@pursuit.org>
2023-04-26 22:44:51 -0400

    First Commit

--- index.js
+++ index.js
@@ -4,7 +4,17 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator ()
+{
+    let result;
+    if (process.argv[2] !== "plus" || process.argv[2] !== "minus") {
+        result = "No operation provided..."
+    }
+    if ( process.argv[ 3 ] && process.argv[ 4 ] === NaN ) {
+       result = "No numbers provided..."
+    } 
+    return result
+}
 
 // Don't change anything below this line.
 module.exports = calculator;
