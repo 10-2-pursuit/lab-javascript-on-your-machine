b53739e2573e29ab30522b70fb61a0e06c3de251
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-04-29 09:58:52 -0400

    Calculator function takes in number and either add or subracts them based on command inputted.

--- index.js
+++ index.js
@@ -4,7 +4,36 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+
+// create variables for numbers/commands
+// variables have to be added to process.argv array
+// function should take a command, either plus or minus
+// function should take numbers, numbers will be added or subracted to one another depending on command input
+
+
+function calculator() {
+    let result = Number(process.argv[3])
+    let command = process.argv[2];
+    if (!command) {
+        result = "No operation provided..."
+    }
+
+    if (!result){
+        result = "No numbers provided..."
+     }
+    for (let i = 4; i < process.argv.length; i++) {
+        if (command === "plus") {
+            result += Number(process.argv[i])
+          }
+        else if (command === "minus") {
+            result -= Number(process.argv[i])
+          }
+        else {
+            result = `Invalid operation: ${process.argv[2]}`
+         } 
+    }
+    return result
+}
 
 // Don't change anything below this line.
 module.exports = calculator;
