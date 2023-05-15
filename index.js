2a6cdf5084ad0fee787561efc3e75c2d19c075fb
Elisaul Batista <elisaulbatista@Elisauls-MacBook-Air.local>
2023-05-15 11:35:08 -0400

    Done

--- index.js
+++ index.js
@@ -4,7 +4,32 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+  const calc = process.argv[2];
+  const num = process.argv.slice(3).map(Number);
+
+  if (calc === undefined) {
+    return "No operation provided...";
+  }
+
+  if (calc !== "plus" && calc !== "minus") {
+    return `Invalid operation: ${calc}`;
+  }
+
+  if (num.length === 0) {
+    return "No numbers provided...";
+  }
+
+  const calcSum = num.reduce((accumulator, currentValue) => {
+    if (calc === "plus") {
+      return accumulator + currentValue;
+    } else if (calc === "minus") {
+      return accumulator - currentValue;
+    }
+  });
+
+  return calcSum;
+}
 
 // Don't change anything below this line.
 module.exports = calculator;
